1123b9746b4ba1df6db27c4b80dbe32c5073bc38
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-10-07 09:54:38 -0300

    Projeto iniciado

--- src/br/senai/sp/jandira/jokenpo/Jogando.java
+++ src/br/senai/sp/jandira/jokenpo/Jogando.java
@@ -1,4 +1,18 @@
 package br.senai.sp.jandira.jokenpo;
 
+import br.senai.sp.jandira.jokenpo.model.IniciandoJogo;
+
+import java.util.Scanner;
+
 public class Jogando {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------");
+        System.out.println("Vamos jogar Jokenpô!");
+        System.out.println("----------------------------------------");
+
+        IniciandoJogo iniciandoJogo = new IniciandoJogo();
+        iniciandoJogo.obterDados();
+
+        iniciandoJogo.iniciarJogo();
+    }
 }
--- src/br/senai/sp/jandira/jokenpo/model/IniciandoJogo.java
+++ src/br/senai/sp/jandira/jokenpo/model/IniciandoJogo.java
@@ -1,4 +1,77 @@
 package br.senai.sp.jandira.jokenpo.model;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class IniciandoJogo {
+    String escolhaUsuario;
+    String escolhaDaMaquina;
+    String pedra;
+    String papel;
+    String tesoura;
+    int numeroSelecionado;
+
+
+    public void obterDados() {
+        Scanner leitor = new Scanner(System.in);
+
+        Random random = new Random();
+        int escolhaPc = random.nextInt(3) + 1;
+
+        System.out.print("Escolha uma opção: ");
+        escolhaUsuario = leitor.nextLine();
+
+        System.out.println("(1) Pedra");
+        System.out.println("(2) Papel");
+        System.out.println("(3) Tesoura");
+        System.out.println("----------------------------------------");
+
+        System.out.print("Digite a sua escolha: ");
+        numeroSelecionado = leitor.nextInt();
+
+        if (numeroSelecionado == 1)
+            escolhaUsuario = "pedra";
+
+        else if (numeroSelecionado == 2) {
+            escolhaUsuario = "papel";
+
+        } else if (numeroSelecionado == 3) {
+            escolhaUsuario = "tesoura";
+        }
+
+        if (escolhaPc == 1)
+            escolhaDaMaquina = "pedra";
+
+        else if (escolhaPc == 2) {
+            escolhaDaMaquina = "papel";
+
+        } else {
+            escolhaDaMaquina = "tesoura";
+        }
+
+
+        System.out.println("Você escolheu: " + escolhaUsuario);
+        System.out.println("O computador escolheu: " + escolhaDaMaquina);
+
+
+    }
+
+    public void iniciarJogo() {
+        if (escolhaUsuario == escolhaDaMaquina) {
+            System.out.println("EMPATE!");
+
+        } else if (escolhaDaMaquina == pedra && escolhaUsuario == tesoura) {
+            System.out.println("VOCÊ PERDEU!");
+
+        } else if (escolhaDaMaquina == papel && escolhaUsuario == pedra) {
+            System.out.println("VOCÊ PERDEU!");
+
+        } else if (escolhaDaMaquina == tesoura && escolhaUsuario == papel) {
+            System.out.println("VOCÊ PERDEU!");
+
+        } else if (escolhaDaMaquina == papel && escolhaUsuario == tesoura) {
+            System.out.println("VOCÊ VENCEU!");
+
+        }
+    }
 }
